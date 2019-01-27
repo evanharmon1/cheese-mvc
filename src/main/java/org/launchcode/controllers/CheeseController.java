3db3eef19b16118cbd4497210a59d11809fd965b
evanharmon1 <evanharmon1@gmail.com>
2019-01-27 15:31:39 -0600

    Finish Studio 4 + bonus missions via Apache Commons .isAlphaSpace. Add apache commons library to project.

--- src/main/java/org/launchcode/controllers/CheeseController.java
+++ src/main/java/org/launchcode/controllers/CheeseController.java
@@ -5,7 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-
+import org.apache.commons.lang3.StringUtils;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -30,9 +30,10 @@ public class CheeseController {
 
     // Request path: cheese/add
     @RequestMapping(value = "add", method = RequestMethod.GET)
-    public String displayAddCheeseForm(Model model) {
+    public String displayAddCheeseForm(Model model, @RequestParam(value="error", required = false) String error) {
 
         model.addAttribute("title", "Add Cheese");
+        model.addAttribute("error", error);
         return "cheese/add";
     }
 
@@ -40,10 +41,53 @@ public class CheeseController {
     @RequestMapping(value = "add", method = RequestMethod.POST)
     public String processAddCheeseForm(@RequestParam String cheeseName, @RequestParam String cheeseDescription) {
 
+        if (cheeseName.isEmpty()) {
+            String errorMessage = "Name field can't be blank";
+            return "redirect:/cheese/add?error=" + errorMessage;
+        }
+        if (!StringUtils.isAlphaSpace(cheeseName)) {
+            String errorMessage = "Name can contain only alphabetic characters and spaces";
+            return "redirect:/cheese/add?error=" + errorMessage;
+        }
+
         cheeses.put(cheeseName, cheeseDescription);
 
         // Redirect to cheese/
         return "redirect:";
     }
 
+    @RequestMapping(value = "remove")
+    public String displayRemoveCheese(Model model) {
+        model.addAttribute("title", "Remove Cheese");
+        model.addAttribute("cheeses", cheeses);
+        return "cheese/remove";
+    }
+
+    // Remove cheeses via select dropdown assignment goal
+    @RequestMapping(value = "remove", method = RequestMethod.POST)
+    public String removeCheese(@RequestParam String cheeseRemove) {
+        cheeses.remove(cheeseRemove);
+        return "redirect:";
+    }
 }
+
+/*
+Remove cheeses via checkboxes assignment goal using Thymeleaf template form:
+<form method="POST">
+    <input th:each="cheese : ${cheeses}" type="checkbox" name="cheesesRemove" th:value="${cheese.key}" th:text="${cheese.key + ' ' + cheese.value}" />
+    <input type="submit" value="Remove" />
+</form>
+
+    @RequestMapping(value = "remove", method = RequestMethod.POST)
+    public String removeCheese(@RequestParam ArrayList<String> cheesesRemove) {
+        for (String cheese : cheesesRemove) {
+            cheeses.remove(cheese);
+        }
+
+        return "redirect:";
+    }
+*/
+
+
+
+
